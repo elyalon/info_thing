f7104bc34a52c171f1ef247347253ecc0400b784
Ely <elyalon345@gmail.com>
2024-09-09 21:16:23 +0300

    Quote program path

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eux
-cd "$(realpath "$(dirname $0)")"
+cd "$(realpath "$(dirname "$0")")"
 cargo build --release && mkdir -p ~/.local/bin && cp "./target/release/info_thing" ~/.local/bin
