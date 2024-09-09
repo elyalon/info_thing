98428201561b81aea8f27e19a2f7096b8fa296a2
Ely <elyalon345@gmail.com>
2024-09-09 21:15:48 +0300

    Ensure ~/.local/bin

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eux
 cd "$(realpath "$(dirname $0)")"
-cargo build --release && cp "./target/release/info_thing" ~/.local/bin/
+cargo build --release && mkdir -p ~/.local/bin && cp "./target/release/info_thing" ~/.local/bin
